d84e6cd1c468257e00e59b65e629ddb6be777730
KazumasaOhashi <k.ohashi.0208@gmail.com>
2017-12-21 02:17:33 +0900

    BPプロジェクトに変換，プラグインのビルド済みファイルを追加

--- Ss6PlayerExamples/Source/Ss6PlayerExamples/Ss6PlayerExamples.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Ss6PlayerExamples.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ss6PlayerExamples, "Ss6PlayerExamples" );
