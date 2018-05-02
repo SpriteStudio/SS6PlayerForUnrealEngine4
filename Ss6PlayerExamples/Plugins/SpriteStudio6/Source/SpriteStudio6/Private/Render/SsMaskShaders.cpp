c34386c3232deaf99e75e0ec3750a045907ac326
KazumasaOhashi <k.ohashi.0208@gmail.com>
2018-05-02 15:58:25 +0900

    マスク用シェーダを追加

---
+++ Ss6PlayerExamples/Plugins/SpriteStudio6/Source/SpriteStudio6/Private/Render/SsMaskShaders.cpp
@@ -0,0 +1,30 @@
+﻿#include "SpriteStudio6PrivatePCH.h"
+#include "SsMaskShaders.h"
+
+#include "ShaderParameterUtils.h"
+
+
+IMPLEMENT_SHADER_TYPE(, FSsMaskVS, TEXT("/Plugin/SpriteStudio6/Private/Ss6MaskShader.usf"), TEXT("MainVS"), SF_Vertex);
+IMPLEMENT_SHADER_TYPE(, FSsMaskPS, TEXT("/Plugin/SpriteStudio6/Private/Ss6MaskShader.usf"), TEXT("MainPS"), SF_Pixel);
+
+
+//
+FSsMaskPS::FSsMaskPS(const ShaderMetaType::CompiledShaderInitializerType& Initializer)
+	: FGlobalShader(Initializer)
+{
+	CellTextureParameter.Bind(Initializer.ParameterMap, TEXT("CellTexture"));
+	CellTextureParameterSampler.Bind(Initializer.ParameterMap, TEXT("CellTextureSampler"));
+}
+bool FSsMaskPS::Serialize(FArchive& Ar)
+{
+	bool bShaderHasOutdatedParams = FGlobalShader::Serialize(Ar);
+
+	Ar << CellTextureParameter;
+	Ar << CellTextureParameterSampler;
+
+	return bShaderHasOutdatedParams;
+}
+void FSsMaskPS::SetCellTexture(FRHICommandList& RHICmdList, const FTextureRHIParamRef InTexture, const FSamplerStateRHIRef SamplerState )
+{
+	SetTextureParameter(RHICmdList, GetPixelShader(), CellTextureParameter, CellTextureParameterSampler, SamplerState, InTexture );
+}
---
+++ Ss6PlayerExamples/Plugins/SpriteStudio6/Source/SpriteStudio6/Private/Render/SsMaskShaders.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "GlobalShader.h"
+#include "UniformBuffer.h"
+#include "RHICommandList.h"
+
+
+// 
+class FSsMaskVS : public FGlobalShader
+{
+	DECLARE_SHADER_TYPE(FSsMaskVS, Global);
+
+public:
+	static bool ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters){ return true; }
+
+	FSsMaskVS(const ShaderMetaType::CompiledShaderInitializerType& Initializer)
+		: FGlobalShader(Initializer)
+	{}
+	FSsMaskVS() {}
+};
+
+// 
+class FSsMaskPS : public FGlobalShader
+{
+	DECLARE_SHADER_TYPE(FSsMaskPS, Global);
+
+public:
+	static bool ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters){ return true; }
+
+	FSsMaskPS(const ShaderMetaType::CompiledShaderInitializerType& Initializer);
+	FSsMaskPS() {}
+
+	virtual bool Serialize(FArchive& Ar) override;
+
+	void SetCellTexture(FRHICommandList& RHICmdList, const FTextureRHIParamRef InTexture, const FSamplerStateRHIRef SamplerState);
+
+private:
+	FShaderResourceParameter CellTextureParameter;
+	FShaderResourceParameter CellTextureParameterSampler;
+};
